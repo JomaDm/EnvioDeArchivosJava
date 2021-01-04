da4ab27287941dcc25426e054547843539cf4704
Joma Dm <Joma_Dm@outlook.com>
2021-01-03 22:07:44 -0600

    Se configuro archivo recepcionArch.java como servidor

--- src/envioarch/RecepcionArch.java
+++ src/envioarch/RecepcionArch.java
@@ -1,9 +1,11 @@
 package envioarch;
+//189.216.151.20
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RecepcionArch {
@@ -11,8 +13,9 @@ public class RecepcionArch {
   public static void main(String[] args) {
     try {
       int puerto = 8000;
-      String ip = "189.216.151.20";
-      String carpeta = "/archivos_recibidos";
+      //String ip = "localhost";
+      ServerSocket ss = new ServerSocket(puerto);
+      String carpeta = "./archivos_recibidos";
       File directorio = new File(carpeta);
       if (!directorio.exists()) {
         if (directorio.mkdirs()) {
@@ -22,30 +25,39 @@ public class RecepcionArch {
           System.out.println("Error al crear directorio");
         }
       }
-      Socket socket = new Socket(ip, puerto);
-      DataInputStream dis = new DataInputStream(socket.getInputStream());
+      while (true) {
+        Socket socket = ss.accept();
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        int numero_archivos = dis.readInt();
+        
+        int tam_buffer = dis.readInt();
+        byte[] b = new byte[tam_buffer];
 
-      int tam_buffer = dis.readInt();
-      byte[] b = new byte[tam_buffer];
+        for (int i = 0; i < numero_archivos; i++) {
+          String nombre_archivo = dis.readUTF();
+          DataOutputStream dos = new DataOutputStream(new FileOutputStream(carpeta + "/" + nombre_archivo));
+          long tam_archivo = dis.readLong();
+          long recibidos = 0;
+          int n, porcentaje;
 
-      int numero_archivos = dis.readInt();
-
-      for (int i = 0; i < numero_archivos; i++) {
-        String nombre_archivo = dis.readUTF();
-        DataOutputStream dos = new DataOutputStream(new FileOutputStream(carpeta + "/" + nombre_archivo));
-        long tam_archivo = dis.readLong();
-        long recibidos = 0;
-        int n, porcentaje;
-
-        while (recibidos < tam_archivo) {
-          n = dis.read(b);
-          dos.write(b, 0, n);
-          dos.flush();
-          recibidos = recibidos + n;
-          porcentaje = (int) (recibidos * 100 / tam_archivo);
-          System.out.print("Recibido: " + porcentaje + "%\r");
-        }//Whil
-        System.out.print("\nArchivo " + nombre_archivo + " recibido.");
+          while (recibidos < tam_archivo) {
+            n = dis.read(b);
+            dos.write(b, 0, n);
+            dos.flush();
+            recibidos = recibidos + n;
+            porcentaje = (int) (recibidos * 100 / tam_archivo);
+            System.out.print("Recibido: " + porcentaje + "%\r");
+            if(recibidos == tam_archivo){
+              System.out.println("\nArchivo " + nombre_archivo + " recibido.");              
+            }
+          }//While
+          dos.close();
+          
+        }
+        
+        dis.close();
+        socket.close();
+        
       }
 
     } catch (Exception e) {
