34b2457b7dcd77576472bcfdc323c0906fb47e75
Joma Dm <Joma_Dm@outlook.com>
2021-01-04 22:25:35 -0600

    Algoritmo de compresion y descompresion implementado, se envia 1 archivo en lugar de muchos archivos

--- src/envioarch/EnvioArch.java
+++ src/envioarch/EnvioArch.java
@@ -6,8 +6,13 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
@@ -16,97 +21,136 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class EnvioArch {
-    //public static String host_default = "201.124.122.167";
-    public static String host_default = "127.0.0.1";
-    public static int puerto_default = 8000; 
-    
-    public static void main(String[] args) {
-        try {
-            String host = host_default;
-            int pto = puerto_default;
-            
-            String datos[] = obtenerDatos();
-            int numBytes = Integer.parseInt(datos[0]);
-            String nagle = datos[1];
-            System.out.println("Bytes: "+numBytes);
-            System.out.println("Nagle: "+nagle);
-            Socket cl = new Socket(host, pto);
-            
+  //public static String host_default = "201.124.122.167";
 
-            JFileChooser jf = new JFileChooser();
-            jf.setMultiSelectionEnabled(true);
-            int r = jf.showOpenDialog(null);
-            File[] files = jf.getSelectedFiles();
-                        
-            if (r == JFileChooser.APPROVE_OPTION) {
-                int numArchivos = files.length;
-                System.out.println("Numero de Archivos: "+numArchivos);   
-                DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
-                //mandamos un mensaje con parametros
-                dos.writeInt(numArchivos);
-                dos.flush();
-                dos.writeInt(numBytes);
-                dos.flush();
-                    
-                //mandamos los archivos
-                for(int i=0;i<files.length;i++){                 
-                    String archivo = files[i].getAbsolutePath(); //Dirección
-                    String nombre = files[i].getName().toString(); //Nombre
-                    long tam = files[i].length();  //Tamaño
-   
-                    System.out.println("Name: "+nombre);
-                    System.out.println("Path: "+archivo);
-                    
-                    dos.writeUTF(nombre);
-                    dos.flush();
-                    dos.writeLong(tam);
-                    dos.flush();
-                    
-                    DataInputStream dis = new DataInputStream(new FileInputStream(archivo));
-                    byte[] b = new byte[numBytes];
-                    long enviados = 0;
-                    int porcentaje, n;
-                    while (enviados < tam) {
-                        n = dis.read(b);
-                        dos.write(b, 0, n);
-                        dos.flush();
-                        enviados = enviados + n;
-                        porcentaje = (int) (enviados * 100 / tam);
-                        System.out.print("Enviado: " + porcentaje + "%\r");
-                    }
-                    System.out.print("\nArchivo #"+(i+1)+" enviado\n\n");
-                    dis.close();                    
-                    //dos.flush();
-                }
-                dos.close();
-                cl.close();    
-            }
-        } catch (Exception e) {
-          e.printStackTrace();
+  public static String host_default = "127.0.0.1";
+  public static int puerto_default = 8000;
+
+  public static void main(String[] args) {
+    try {
+      String host = host_default;
+      int pto = puerto_default;
+
+      String datos[] = obtenerDatos();
+      int numBytes = Integer.parseInt(datos[0]);
+      String nagle = datos[1];
+      System.out.println("Bytes: " + numBytes);
+      System.out.println("Nagle: " + nagle);
+      Socket cl = new Socket(host, pto);
+
+      JFileChooser jf = new JFileChooser();
+      jf.setMultiSelectionEnabled(true);
+      int r = jf.showOpenDialog(null);
+      File[] files = jf.getSelectedFiles();
+
+      if (r == JFileChooser.APPROVE_OPTION) {
+        int numArchivos = files.length;
+        System.out.println("Numero de Archivos: " + numArchivos);
+        DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+
+        File zip = comprimirArchivos(files);
+        dos.writeInt(numBytes); //No bytes
+        dos.flush();       
+        
+        long tam = zip.length();  //Tamaño
+        dos.writeLong(tam);
+        dos.flush();
+        
+        dos.writeUTF(zip.getName()); //Nombre
+        dos.flush();
+        
+        DataInputStream dis = new DataInputStream(new FileInputStream(zip));
+        byte[] b = new byte[numBytes];
+        long enviados = 0;
+        int porcentaje, n;
+        while (enviados < tam) {
+          n = dis.read(b);
+          dos.write(b, 0, n);
+          dos.flush();
+          enviados = enviados + n;
+          porcentaje = (int) (enviados * 100 / tam);
+          System.out.print("Enviado: " + porcentaje + "%\r");          
         }
+        System.out.print("\nArchivos enviado\n\n");
+        dis.close();
+        dos.close();
+        cl.close();
+        
+        zip.deleteOnExit();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    
-    private static String[] obtenerDatos() {
-        String s[]=new String[2];
+  }
 
-        JTextField bytes = new JTextField(20);
-        JCheckBox check = new JCheckBox("Nagle",true);
-        bytes.setText("1024");
-        
-        JPanel myPanel = new JPanel();
-        myPanel.setLayout(new GridLayout(2, 2));
-        myPanel.add(new JLabel("Bytes en buffer:"));
-        myPanel.add(bytes);
-        myPanel.add(new JLabel("¿Usar Nagle?:"));
-        myPanel.add(check);
-        
-        int result = JOptionPane.showConfirmDialog(null, myPanel, 
-                 "Configuración de comunicación", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-                s[0] = bytes.getText();
-                if(check.isSelected()) s[1] = "SI";
-                else s[1] = "NO";
-        }else System.exit(0);
-        return s;
-    }    
+  private static File comprimirArchivos(File[] archivos) {
+    String zipFile = archivos[0].getPath().replace(archivos[0].getName(), "") + "temp.zip";
+    
+    try {
+
+      // create byte buffer
+      byte[] buffer = new byte[65536];
+
+      FileOutputStream fos = new FileOutputStream(zipFile);
+
+      ZipOutputStream zos = new ZipOutputStream(fos);
+
+      for (int i = 0; i < archivos.length; i++) {
+
+        //File srcFile = new File(srcFiles[i]);
+        FileInputStream fis = new FileInputStream(archivos[i]);
+
+        // begin writing a new ZIP entry, positions the stream to the start of the entry data
+        zos.putNextEntry(new ZipEntry(archivos[i].getName()));
+
+        int length;
+
+        while ((length = fis.read(buffer)) > 0) {
+          zos.write(buffer, 0, length);
+        }
+
+        zos.closeEntry();
+
+        // close the InputStream
+        fis.close();
+
+      }
+
+      // close the ZipOutputStream
+      zos.close();
+
+    } catch (IOException ioe) {
+      System.out.println("Error creating zip file: " + ioe);
+    }
+    return new File(zipFile);
+  }
+
+  private static String[] obtenerDatos() {
+    String s[] = new String[2];
+
+    JTextField bytes = new JTextField(20);
+    JCheckBox check = new JCheckBox("Nagle", true);
+    bytes.setText("1024");
+
+    JPanel myPanel = new JPanel();
+    myPanel.setLayout(new GridLayout(2, 2));
+    myPanel.add(new JLabel("Bytes en buffer:"));
+    myPanel.add(bytes);
+    myPanel.add(new JLabel("¿Usar Nagle?:"));
+    myPanel.add(check);
+
+    int result = JOptionPane.showConfirmDialog(null, myPanel,
+            "Configuración de comunicación", JOptionPane.OK_CANCEL_OPTION);
+    if (result == JOptionPane.OK_OPTION) {
+      s[0] = bytes.getText();
+      if (check.isSelected()) {
+        s[1] = "SI";
+      } else {
+        s[1] = "NO";
+      }
+    } else {
+      System.exit(0);
+    }
+    return s;
+  }
 }
--- src/envioarch/RecepcionArch.java
+++ src/envioarch/RecepcionArch.java
@@ -4,9 +4,15 @@ package envioarch;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class RecepcionArch {
 
@@ -31,40 +37,69 @@ public class RecepcionArch {
         System.out.println("Conexión establecida desde" + socket.getInetAddress() + ":" + socket.getPort());
         DataInputStream dis = new DataInputStream(socket.getInputStream());
 
-        int numero_archivos = dis.readInt();
-        System.out.println("Numero de archivos: " + numero_archivos);
         int tam_buffer = dis.readInt();
         System.out.println("Tamaño del buffer: " + tam_buffer);
 
+        long tam_archivo = dis.readLong();
+        System.out.println("Tamaño del archivo: " + tam_archivo);
+
+        //dis = new DataInputStream(socket.getInputStream());
+        String nombre_archivo = dis.readUTF();
+        System.out.println("Nombre del archivo: " + nombre_archivo);
+
         byte[] b = new byte[tam_buffer];
 
-        for (int i = 0; i < numero_archivos; i++) {
-          String nombre_archivo = dis.readUTF().trim();
-          System.out.println("Nombre del archivo " + (i + 1) + ": " + nombre_archivo);
+        long recibidos = 0;
+        int n, porcentaje;
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(carpeta + "/" + nombre_archivo));
+        while (recibidos < tam_archivo) {
+          n = dis.read(b);
+          dos.write(b, 0, n);
+          dos.flush();
+          recibidos = recibidos + n;
+          porcentaje = (int) (recibidos * 100 / tam_archivo);
+          System.out.print("Recibido: " + porcentaje + "%\r");
+        }//While                    
+        System.out.println("Archivo " + nombre_archivo + " Recibido");
+        dos.close();
+        dis.close();
 
-          long tam_archivo = dis.readLong();
-          System.out.println("Tamaño del archivo: " + tam_archivo);
-          long recibidos = 0;
-          int n, porcentaje;
-          DataOutputStream dos = new DataOutputStream(new FileOutputStream(carpeta + "/" + nombre_archivo));
-          while (recibidos < tam_archivo) {
-            n = dis.read(b);
-            dos.write(b, 0, n);
-            dos.flush();
-            recibidos = recibidos + n;
-            porcentaje = (int) (recibidos * 100 / tam_archivo);
-            System.out.print("Recibido: " + porcentaje + "%\r");
-          }//While
-          //dis.read();
-          System.out.println("Archivo " + nombre_archivo + " Recibido");
-
-          dos.close();
-        }
+        descomprimirArchivos(new File("temp.zip"), carpeta);
 
       }
 
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  public static void descomprimirArchivos(File zip, String carpeta) {
+    String directorioZip = carpeta + "/";
+    try {
+      //crea un buffer temporal para el archivo que se va descomprimir
+      ZipInputStream zis = new ZipInputStream(new FileInputStream(directorioZip + zip.getName()));
+
+      ZipEntry salida;
+      //recorre todo el buffer extrayendo uno a uno cada archivo.zip y creándolos de nuevo en su archivo original 
+      while (null != (salida = zis.getNextEntry())) {
+        System.out.println("Nombre del Archivo: " + salida.getName());
+        FileOutputStream fos = new FileOutputStream(directorioZip + salida.getName());
+        int leer;
+        byte[] buffer = new byte[65536];
+        while (0 < (leer = zis.read(buffer))) {
+          fos.write(buffer, 0, leer);
+        }
+        fos.close();
+        zis.closeEntry();
+      }     
+      zis.close();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      File z  = new File(directorioZip + zip.getName());
+      z.delete();
+    }
+
   }
 }
